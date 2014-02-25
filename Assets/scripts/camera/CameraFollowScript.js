994867b3f5cea26c1a0afb66e08b643acb19f292
Alex Coady <alexandercoady@me.com>
2014-02-25 23:03:36 +0000

    Added check for destroyed character after game over

--- Assets/scripts/camera/CameraFollowScript.js
+++ Assets/scripts/camera/CameraFollowScript.js
@@ -4,6 +4,10 @@ var pop: Transform;
 
 function Update () {
 
+	if (!pop) {
+		return;
+	}
+
 	// Ensures camera moves up with Pop but doesn't ever move down
 	transform.position.y += pop.position.y > transform.position.y ? pop.position.y - transform.position.y : 0;
 }
